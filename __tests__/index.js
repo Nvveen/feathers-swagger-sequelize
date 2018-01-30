88c34449a8bddc961e0a0046a059679b236f6f75
Neal van Veen <neal@wearelandscape.nl>
2018-01-30 12:55:31 +0100

    add swagger to tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,8 +1,12 @@
+import 'babel-polyfill'
 import { expect } from 'chai'
 import Sequelize from 'sequelize'
 import feathers from '@feathersjs/feathers'
 import express from '@feathersjs/express'
 import service from 'feathers-sequelize'
+import swagger from 'feathers-swagger'
+import fetch from 'isomorphic-fetch'
+import generateSwagger from '../lib/index'
 
 describe('feathers-swagger-sequelize', () => {
   describe('basic functionality', () => {
@@ -19,12 +23,24 @@ describe('feathers-swagger-sequelize', () => {
       })
       app = express(feathers())
         .configure(express.rest())
+        .configure(
+          swagger({
+            docsPath: '/docs',
+            info: {
+              title: 'API Documentation',
+              description: 'API Documentation'
+            }
+          })
+        )
+        .configure(generateSwagger)
         .use('/users', service({ Model: User }))
       server = app.listen(12345, () => done())
     })
     after(done => server.close(done))
-    it('should support basis functionality with a simple app', () => {
-      console.log('app', app)
+    it('should support basis functionality with a simple app', async () => {
+      const res = await fetch('http://localhost:12345/docs')
+      const data = await res.json()
+      console.log('data', data)
     })
   })
 })
