ce87eb7526ecb88842107784fb4b5a0b7694d0eb
Neal van Veen <neal@wearelandscape.nl>
2018-01-30 14:38:28 +0100

    check other fields in ut

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,5 +1,6 @@
 import 'babel-polyfill'
-import { expect } from 'chai'
+import chai, { expect } from 'chai'
+import chaiThings from 'chai-things'
 import Sequelize from 'sequelize'
 import feathers from '@feathersjs/feathers'
 import express from '@feathersjs/express'
@@ -7,6 +8,8 @@ import service from 'feathers-sequelize'
 import swagger from 'feathers-swagger'
 import fetch from 'isomorphic-fetch'
 import generateSwagger from '../lib/index'
+chai.should()
+chai.use(chaiThings)
 
 describe('feathers-swagger-sequelize', () => {
   describe('basic functionality', () => {
@@ -17,10 +20,22 @@ describe('feathers-swagger-sequelize', () => {
         storage: ':memory:',
         dialect: 'sqlite'
       })
-      const User = sequelize.define('users', {
-        email: Sequelize.STRING,
-        password: Sequelize.STRING
-      })
+      const User = sequelize.define(
+        'users',
+        {
+          email: {
+            type: Sequelize.STRING,
+            description: 'An email-adres'
+          },
+          password: {
+            type: Sequelize.STRING,
+            description: 'A password'
+          }
+        },
+        {
+          description: 'A user'
+        }
+      )
       app = express(feathers())
         .configure(express.rest())
         .configure(
@@ -32,15 +47,30 @@ describe('feathers-swagger-sequelize', () => {
             }
           })
         )
-        .configure(generateSwagger)
         .use('/users', service({ Model: User }))
+        .configure(generateSwagger)
       server = app.listen(12345, () => done())
     })
     after(done => server.close(done))
     it('should support basis functionality with a simple app', async () => {
       const res = await fetch('http://localhost:12345/docs')
+      await res.json()
+    })
+    it('should set the description from the model', async () => {
+      const res = await fetch('http://localhost:12345/docs')
       const data = await res.json()
-      console.log('data', data)
+      expect(data).to.have.nested.property(
+        'definitions.users.properties.email.description',
+        'An email-adres'
+      )
+      expect(data).to.have.nested.property(
+        'definitions.users.properties.password.description',
+        'A password'
+      )
+      expect(data)
+        .to.have.property('tags')
+        .that.is.an('array')
+        .to.include.something.that.has.property('description', 'A user')
     })
   })
 })
